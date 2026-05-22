4dafab94cfcae8c8ab27079d0bed011a0cf05896
Nimet Kurnaz <nkurn079@uOttawa.ca>
2026-05-21 20:43:54 -0400

    main commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Everything will be alright.");
+        System.out.println("Everything will work out sooner or later.");
     }
 }
